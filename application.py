2a40e2382e2d59f08a196f0c155903475beb7658
Viktor Savelev <viktor.savelev@3d4medical.com>
2018-09-25 20:46:44 +0300

    Init

---
+++ application.py
@@ -0,0 +1,51 @@
+import os
+
+from flask import Flask, session, render_template, request
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/reg_form")
+def reg_form():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+	email = request.form.get('email')
+	password = request.form.get('password')
+
+	db.execute("INSERT INTO users (email, password) VALUES ('{}', '{}')".format(email, password))
+	db.commit()
+	return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+	pass
+
+@app.route('/logout', methods=['POST'])
+def logout():
+	pass
+
+
+
+
